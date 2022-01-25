b354750dad7b92629a92e768c3fdbc9ba8935574
ZawadiSumba66 <rosesumba98@gmail.com>
2022-01-25 11:34:53 +0300

    Implement local storage to persist data

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,34 @@
-import React, { createContext, useContext, useState, useCallback } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+type AppData = {
+  moodList: MoodOptionWithTimestamp[];
+};
+
+const dataKey = 'my-app-data';
+
+const setAppData = async (appData: AppData): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(dataKey, JSON.stringify(appData));
+  } catch {}
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const result = await AsyncStorage.getItem(dataKey);
+    if (result) {
+      return JSON.parse(result);
+    }
+  } catch {}
+  return null;
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -14,10 +43,24 @@ const AppContext = createContext<AppContextType>({
 export const AppProvider: React.FC = ({ children }) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
   const handleSelectMood = useCallback((selectedMood: MoodOptionType) => {
-    setMoodList((list) => [
-      ...list,
-      { mood: selectedMood, timestamp: Date.now() },
-    ]);
+    setMoodList((list) => {
+      const newMoodList = [
+        ...list,
+        { mood: selectedMood, timestamp: Date.now() },
+      ];
+      setAppData({ moodList: newMoodList });
+      return newMoodList;
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const data = await getAppData();
+      if (data) {
+        setMoodList(data.moodList);
+      }
+    };
+    fetchAppData();
   }, []);
   return (
     <AppContext.Provider value={{ moodList, handleSelectMood }}>
